a7597aecab50740c73ee089f62d8994200c4b0cb
Md Saimum Al Mahmud <saimumadi00@github.com>
2026-05-28 04:38:11 +0000

    Security: HTTP headers via vercel.json, disable source maps, nosniff+referrer meta, .env gitignore

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     chunkSizeWarningLimit: 650,
+    sourcemap: false,
     rolldownOptions: {
       checks: {
         pluginTimings: false,
